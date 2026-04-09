097ff3e14f89b6685e70ddac192bb6280f311d9d
diether18 <jagielo.adrian@gmail.com>
2026-04-09 21:05:50 +0200

    feature-1/ create project and init commit

--- src/main/kotlin/config/HTTP.kt
+++ src/main/kotlin/config/HTTP.kt
@@ -1,12 +1,11 @@
 package com.anjo.config
 
-import io.ktor.http.*
-import io.ktor.openapi.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.defaultheaders.*
-import io.ktor.server.plugins.swagger.*
-import io.ktor.server.routing.*
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpMethod
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.plugins.cors.routing.CORS
+import io.ktor.server.plugins.defaultheaders.DefaultHeaders
 
 fun Application.configureHTTP() {
     install(CORS) {
