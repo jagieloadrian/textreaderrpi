8e019abd2c37dec40b86338dca89c80745a3ed26
diether18 <jagielo.adrian@gmail.com>
2026-07-07 21:07:57 +0200

    fix(19): WR-02 add parseFilter unit tests pinning sanitize-before-blank-check ordering

--- src/test/kotlin/com/anjo/validation/HistoryValidatorsTest.kt
+++ src/test/kotlin/com/anjo/validation/HistoryValidatorsTest.kt
@@ -2,6 +2,7 @@ package com.anjo.validation
 
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
+import io.ktor.http.parametersOf
 
 class HistoryValidatorsTest : FunSpec({
 
@@ -23,5 +24,21 @@ class HistoryValidatorsTest : FunSpec({
 
     test("sanitizeSearchTerm strips multiple mixed wildcards") {
         HistoryValidators.sanitizeSearchTerm("a%b_c%d") shouldBe "abcd"
+    }
+
+    test("parseFilter wildcard-only search yields null search filter") {
+        HistoryValidators.parseFilter(parametersOf("search", "%_%")).search shouldBe null
+    }
+
+    test("parseFilter sanitizes search before the blank check") {
+        HistoryValidators.parseFilter(parametersOf("search", "a%b")).search shouldBe "ab"
+    }
+
+    test("parseFilter effect ALL in any case yields null effect filter") {
+        HistoryValidators.parseFilter(parametersOf("effect", "all")).effect shouldBe null
+    }
+
+    test("parseFilter lowercase effect is uppercased") {
+        HistoryValidators.parseFilter(parametersOf("effect", "scroll")).effect shouldBe "SCROLL"
     }
 })
